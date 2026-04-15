c3626b07eca7ee749b3b04e1b48999668acb7602
Tirupatitata123 <tirupati.boyane@gmail.com>
2026-04-15 12:15:57 +0530

    Add default values for Terraform variables

--- variable.tf
+++ variable.tf
@@ -1,12 +1,17 @@
 variable ami_value {
     description = "This is the value of AMI_Id"
+    default = "ami-05d2d839d4f73aafb"
 }
 variable ec2_type {
     description = "This is the value of EC2 Instance Type"
+    default = "t3.micro"
+
 }
 variable tag_value {
     description = "This is the value of Tag Value"
+    default = "newvm"
 }
 variable key {
     description = "This is the value of Key Name"
+    default = "officep"
 }
